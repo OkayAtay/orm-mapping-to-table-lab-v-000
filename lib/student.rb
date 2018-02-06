5574b5098b208167fb8c45969d17bdc1f1de0004
Araba Taylor <araba.taylor20@gmail.com>
2018-02-06 03:28:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,10 @@ class Student
        SQL
    DB[:conn].execute(sql)
   end
+
+  def drop_table
+    
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
